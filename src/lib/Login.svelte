d653ae83c8bc0ac235c00c974fa2d1ef9ec337ad
nosuz <nosuzuki@postcard.st>
2022-09-11 21:10:58 +0900

    Show error message on failed to login.

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -8,10 +8,12 @@
 
   let user_id = "";
   let password = "";
+  let loginFailed = false;
 
   function onSubmit() {
     console.log("Login");
     console.log(user_id, password);
+    loginFailed = false;
 
     var sipUri = "sip:" + user_id + "@" + config.sip_server;
     sipStore.set(sipUri);
@@ -31,6 +33,8 @@
     });
     ua.on("disconnected", function (e) {
       console.log("disconnected");
+      ua.stop();
+      loginFailed = true;
     });
 
     ua.on("registered", function (e) {
@@ -44,6 +48,7 @@
     });
     ua.on("registrationFailed", function (e) {
       console.log("registrationFailed");
+      loginFailed = true;
     });
 
     ua.start();
@@ -52,7 +57,27 @@
 
 <div>
   <h2>Login</h2>
-  <form>
+  {#if loginFailed}
+    <div class="alert alert-warning d-flex align-items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-exclamation-circle"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+        />
+        <path
+          d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"
+        />
+      </svg>
+      <div>Login failed</div>
+    </div>
+  {/if}
+  <form on:submit|preventDefault={onSubmit}>
     <div class="form-floating mb-3">
       <input
         type="text"
@@ -76,13 +101,7 @@
     </div>
 
     <div class="row mb-3">
-      <button
-        type="button"
-        class="btn btn-primary col-sm-2"
-        on:click={onSubmit}
-      >
-        Login
-      </button>
+      <button type="submit" class="btn btn-primary col-sm-2"> Login </button>
     </div>
   </form>
 </div>
